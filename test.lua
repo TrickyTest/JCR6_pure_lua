1b715762dcb85c16fc6470e57479cd3774e0ca93
Tricky1975 <facebook@tbbs.nl>
2019-02-06 14:33:55 +0100

    Serial test

--- test.lua
+++ test.lua
@@ -1,2 +1,36 @@
 require "JCR6_zlib"
-d = JCR_Dir("MKL_test.jcr") -- I didn't include this file in the repository for obvious reasons, I believe :P
+
+local io = io
+local pairs = pairs
+local print = print
+
+JCR_Chat = true -- This is a test after all!
+
+print("Reading")
+local d = JCR_Dir("MKL_test.jcr") -- I didn't include this file in the repository for obvious reasons, I believe :P
+
+
+
+print("Let's see the results")
+
+
+local function serie(t,tabs)
+    tabs = tabs or 0
+    for k,v in pairs(t) do
+        for i=1,tabs do io.write("\t") end
+        io.write( ("%s %s = "):format(type(v),k) )
+        if type(v)=="table" then
+           print("")
+           serie(v,tabs+1)
+        elseif type(v)=="function" then
+           print("< FUNC >")
+        else
+           print(v)
+        end         
+    end
+    print("")
+end
+
+for e in d:EntryList() do
+    print(e)
+end    
